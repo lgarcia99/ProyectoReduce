e39cd2aa27ce158b6352731808dd73614b0b65a8
Ana Sofia Salgado <anasalgadomontoya@gmail.com>
2020-03-28 19:55:42 -0700

    Avance con todos los mockups y colores

---
+++ app/src/main/java/salgadomontoya/anasofia/reduce/Datos.kt
@@ -0,0 +1,12 @@
+package salgadomontoya.anasofia.reduce
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Datos : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_datos)
+    }
+}
