bc77d3bb7d0eafafb3ec33c78407402717979256
Ana Sofia Salgado <anasalgadomontoya@gmail.com>
2020-03-31 21:55:09 -0700

    Añadido panel de escribir articulo y correcciones a varias pantallas

---
+++ app/src/main/java/salgadomontoya/anasofia/reduce/detalle_articulo.kt
@@ -0,0 +1,12 @@
+package salgadomontoya.anasofia.reduce
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class detalle_articulo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle_articulo)
+    }
+}
