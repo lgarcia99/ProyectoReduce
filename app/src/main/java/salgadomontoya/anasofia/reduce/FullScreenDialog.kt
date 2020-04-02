bda7e9ba85c8c726e10c7c72a12f5b84c7af8107
lgarcia99 <54911648+lgarcia99@users.noreply.github.com>
2020-04-01 18:21:39 -0700

    Pantalla de Agregar actividad

---
+++ app/src/main/java/salgadomontoya/anasofia/reduce/FullScreenDialog.kt
@@ -0,0 +1,32 @@
+package salgadomontoya.anasofia.reduce
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import kotlinx.android.synthetic.main.layout_full_screen_dialog.*
+
+class FullScreenDialogExample(private val callbackListener: CallbackListener) : DialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        isCancelable = false
+        return inflater.inflate(R.layout.layout_full_screen_dialog, container, false)
+    }
+    override fun getTheme(): Int {
+        return R.style.DialogTheme
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button.setOnClickListener {
+            //send back data to PARENT fragment using callback
+            callbackListener.onDataReceived(editText.text.toString())
+            // Now dismiss the fragment
+            dismiss()
+        }
+
+    }
+}
